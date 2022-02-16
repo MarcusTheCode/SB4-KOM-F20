baa8f24d6b720820e933bf4e3f8dd8a0a33bde07
Marcus Byllemos <MarcusByllemos@gmail.com>
2022-02-16 10:01:27 +0100

    test

--- AsteroidsEntityFramework/Common/src/main/java/dk/sdu/mmmi/cbse/common/services/IGamePluginService.java
+++ AsteroidsEntityFramework/Common/src/main/java/dk/sdu/mmmi/cbse/common/services/IGamePluginService.java
@@ -3,6 +3,10 @@ package dk.sdu.mmmi.cbse.common.services;
 import dk.sdu.mmmi.cbse.common.data.GameData;
 import dk.sdu.mmmi.cbse.common.data.World;
 
+/*
+ *  Starting and stopping the game
+ */
+
 public interface IGamePluginService {
     void start(GameData gameData, World world);
 
